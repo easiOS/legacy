29e8001b01ec859bdcacc652b28d80dd05e9c5a2
Easimer <easimer@gmail.com>
2016-06-16 20:42:48 +0200

    Fix kernel name typo

--- kern/kernel.c
+++ kern/kernel.c
@@ -37,7 +37,7 @@
 
 struct cpu_desc cpu_desc;
 
-#define KERNEL_NAME "easiOS v0.4.0"
+#define KERNEL_NAME "easiOS v0.3.11"
 
 const char* cmdline = NULL;
 uint16_t __attribute__((aligned(4))) text_buffer[2000];
